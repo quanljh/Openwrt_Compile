f765201d41a8daeadc99823cca2a5e3718b63f84
quanljh <quanljh@gmail.com>
2024-04-22 11:09:39 +0800

    fix settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -3,7 +3,7 @@
 STARTUP_SCRIPT=$GITHUB_WORKSPACE/Configs/999-custom-startup.sh
 UCI_DEFAULTS_SCRIPT=./files/etc/uci-defaults/999-custom-startup.sh
 
-mkdir ./files/etc/uci-defaults/
+mkdir -p ./files/etc/uci-defaults/
 cp -rf $STARTUP_SCRIPT $UCI_DEFAULTS_SCRIPT
 
 sed -i "s/lan_ip_address=".*"/lan_ip_address="$WRT_IP"/g" $UCI_DEFAULTS_SCRIPT
