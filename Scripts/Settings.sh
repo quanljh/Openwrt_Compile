443792274543b636335fb352d8105c943fbc7a96
quanljh <quanljh@gmail.com>
2024-07-27 15:47:11 +0800

    Update Setting.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -6,8 +6,8 @@ UCI_DEFAULTS_SCRIPT=./files/etc/uci-defaults/999-custom-startup.sh
 mkdir -p ./files/etc/uci-defaults/
 cp -rf $STARTUP_SCRIPT $UCI_DEFAULTS_SCRIPT
 
-sed -i "s/lan_ip_address=".*"/lan_ip_address="$WRT_IP"/g" $UCI_DEFAULTS_SCRIPT
-sed -i "s/root_password=".*"/root_password="$WRT_PASSWORD"/g" $UCI_DEFAULTS_SCRIPT
+sed -i "s/lan_ip_address=".*"/lan_ip_address=\"$WRT_IP\"/g" $UCI_DEFAULTS_SCRIPT
+sed -i "s/root_password=".*"/root_password=\"$WRT_PASSWORD\"/g" $UCI_DEFAULTS_SCRIPT
 
 echo "Content of $UCI_DEFAULTS_SCRIPT:"
 cat $UCI_DEFAULTS_SCRIPT
