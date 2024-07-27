d0a0919c27506945abb93d717ff855e0b732c25c
quanljh <quanljh@gmail.com>
2024-07-27 17:58:07 +0800

    Add x86 support

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -8,6 +8,3 @@ cp -rf $STARTUP_SCRIPT $UCI_DEFAULTS_SCRIPT
 
 sed -i "s/lan_ip_address=".*"/lan_ip_address=\"$WRT_IP\"/g" $UCI_DEFAULTS_SCRIPT
 sed -i "s/root_password=".*"/root_password=\"$WRT_PASSWORD\"/g" $UCI_DEFAULTS_SCRIPT
-
-echo "Content of $UCI_DEFAULTS_SCRIPT:"
-cat $UCI_DEFAULTS_SCRIPT
