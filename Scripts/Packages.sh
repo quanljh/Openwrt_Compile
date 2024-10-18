7175fcb0edfd08e0c3f598705bc54d00c5456057
quanljh <quanljh@gmail.com>
2024-10-18 16:53:10 +0800

    Update passwall code after clone

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -2,6 +2,7 @@ UPDATE_PACKAGE() {
 	local PKG_NAME=$1
 	local PKG_REPO=$2
 	local PKG_BRANCH=$3
+	local POST_UPDATE_METHOD=$4
 	local REPO_NAME=$(echo $PKG_REPO | cut -d '/' -f 2)
 
 	local PKG_PATH=$(find ./luci/ ./packages/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune)
@@ -20,7 +21,23 @@ UPDATE_PACKAGE() {
 	rm -rf $REPO_NAME
 
 	echo "Package $PKG_NAME is updated."
+
+	if [ -n "$POST_UPDATE_METHOD" ]; then
+		$POST_UPDATE_METHOD "$PKG_PATH"
+	fi
 }
 
+UPDATE_PASSWALL_CODE() {
+	local PKG_PATH=$1
 
-UPDATE_PACKAGE "luci-app-passwall" "xiaorouji/openwrt-passwall" "main"
+	if [ -z "$PKG_PATH" ]; then
+		echo "Package path is not found."
+  		exit 1
+	fi
+
+	sed -i '/_M.xray/,/}/s/get_url = gh_pre_release_url/get_url = gh_release_url/' $PKG_PATH/luasrc/passwall/com.lua
+
+	echo "Passwall code is updated."
+}
+
+UPDATE_PACKAGE "luci-app-passwall" "xiaorouji/openwrt-passwall" "main" "UPDATE_PASSWALL_CODE"
