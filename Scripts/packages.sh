ee01cfd1066e7b5db66c7a4ce764b5e2609e505a
quanljh <quanljh@gmail.com>
2024-12-16 14:48:03 +0800

    Add xray makefile

--- Scripts/packages.sh
+++ Scripts/packages.sh
@@ -5,4 +5,13 @@ update_smartdns() {
 	mv -f $MAKEFILE_SRC $MAKEFILE_DST
 }
 
+update_xray() {
+	local MAKEFILE_SRC="$GITHUB_WORKSPACE/Makefiles/xray/Makefile"
+	local MAKEFILE_DST="./feeds/packages/net/xray-core/Makefile"
+
+	mv -f $MAKEFILE_SRC $MAKEFILE_DST
+}
+
+
 update_smartdns
+update_xray
